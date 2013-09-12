faf6576ac3d7e39455c7ea3fb15e49846cb10e3e
Cyril David <cyx@cyx.is>
2013-09-12 14:37:00 -0700

    Routing change

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.all('*', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
